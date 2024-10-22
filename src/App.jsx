3b57ac4428cb8aad23e5fb5b8dda39cd9f5882c3
Monu_2114 <monu.nanabala@gmail.com>
2024-10-22 23:03:17 +0530

    Display button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
 import Card from "./components/Card";
 import "./App.css";
-
 function App() {
+  //calling api after every render
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.quicksell.co/v1/internal/frontend-assignment"
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData(); // Call the async function
+  }, []);
+
   return (
-    <>
+    <div className="app">
+      <button className="display">
+        <img src="src/assets/Display.svg" alt="" />
+        Display
+        <img src="src/assets/down.svg" alt="" />
+      </button>
+
       <Card
         id="CAM-11"
         avatar="src\assets\koala.jpeg"
@@ -11,7 +34,7 @@ function App() {
         icon="src/assets/koala.jpeg"
         tag="Feature Request"
       />
-    </>
+    </div>
   );
 }
 
