44f8e88e768d76eee305e49e71b416ca7e913a02
Monu_2114 <monu.nanabala@gmail.com>
2024-10-22 23:37:05 +0530

    Dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import Card from "./components/Card";
 import "./App.css";
 function App() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
   //calling api after every render
   useEffect(() => {
     const fetchData = async () => {
@@ -21,12 +25,30 @@ function App() {
 
   return (
     <div className="app">
-      <button className="display">
+      <button className="display" onClick={toggleDropdown}>
         <img src="src/assets/Display.svg" alt="" />
         Display
         <img src="src/assets/down.svg" alt="" />
       </button>
-
+      {showDropdown && (
+        <div className="dropdown">
+          <div className="dropdown-item">
+            <label>Grouping</label>
+            <select>
+              <option value="status">Status</option>
+              <option value="user">User</option>
+              <option value="priority">Priority</option>
+            </select>
+          </div>
+          <div className="dropdown-item">
+            <label>Ordering</label>
+            <select>
+              <option value="priority">Priority</option>
+              <option value="title">Title</option>
+            </select>
+          </div>
+        </div>
+      )}
       <Card
         id="CAM-11"
         avatar="src\assets\koala.jpeg"
